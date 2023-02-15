836b60dfb1de5970eb9bf1128a05583f47d5f49b
Elhombredelanoche <elbrandonpro@yahoo.fr>
2023-02-15 23:09:36 +0100

    Com_Wallet.php & refacto - 1 param

--- src/Domain/Supply/Wallet/Wallet.php
+++ src/Domain/Supply/Wallet/Wallet.php
@@ -12,27 +12,38 @@ Autoloader::register();
 
 class Wallet implements InterfaceWallet
 {
-    protected $amount;
+    protected $amount; // Représente une valeur ajouter ou à retirer
 
+    /**
+     * Constructeur permettant de récupèrer l'Id du joueur et 
+     */
     public function __construct(private int $playerId, public int $balance)
     {
     }
 
-    public function withDrawAmount($amount, $balance): int
+    /**
+     * La fonction withDrawAmount va permettre de retirer un montant du solde du compte.
+     */
+    public function withDrawAmount($amount): int
     {
         if ($this->balance - $amount) {
-            return $this->balance = $balance;
+            return $this->balance;
         }
     }
 
-    public function injectAmount($amount, $balance): int
+    /**
+     * La fonction injectAmount prend un montant $amount en paramétre qui va être déduire du portfeuille du joueur.
+     */
+    public function injectAmount($amount): int
     {
         if ($amount > 0) {
-            return $this->balance + $amount = $balance;
+            return $this->balance + $amount;
         }
     }
-
-    private function isBalandePositive($balance): bool
+    /**
+     * La fonction isBalancePositive permet d'éviter que le solde du compte ne soit pas négatif. 
+     */
+    private function isBalancePositive(): bool
     {
         if ($this->balance < 0) {
             return false;
